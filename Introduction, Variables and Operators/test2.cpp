631826df06bacdea3151f679dab0eeb5fcc126cd
siddharthkush2002 <siddharthkush12@gmail.com>
2023-08-16 09:43:20 +0530

    update

--- Introduction, Variables and Operators/test2.cpp
+++ Introduction, Variables and Operators/test2.cpp
@@ -1,35 +1,15 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     string s;
-//     getline(cin,s);
-//     for (int i = 0; i < s.length();i++)
-//     {
-//         cout<<char(s[i]-'a'+'A');
-//     }
-    
-
-
-//     return 0;
-// }
-
-
-
 #include<iostream>
 using namespace std;
-int count(int n){
-    int rem,rev=0;
-    while(n>0){
-        rem=n%10;
-        rev++;
-        n/=10;
-    }
-    return rev;
-
+// int max(int x,int y){
+//     return (x>y)?x:y;
+// }
+inline int max(int x,int y){
+    return (x>y)?x:y;
 }
 int main(){
-    int n;
-    cin>>n;
-    cout<<count(n);
+    cout<<max(20,10)<<endl;
+    cout<<max(0,200)<<endl;
+    cout<<max(100,1010)<<endl;
+    
     return 0;
-}
+} 
